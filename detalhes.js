d82439b4f0a759f55a739788abc1e94553e1bad1
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-09-03 18:22:36 -0300

    Arquivo detalhes.js adicionado

---
+++ detalhes.js
@@ -0,0 +1,22 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+
+fetch('aparicoes.json')
+    .then(response => response.json())
+    .then(data => {
+        const id = getQueryParam('id');
+        const aparicao = data.Aparicoes.find(a => a.id == id);
+        if (aparicao) {
+            const detalhesAparicao = document.getElementById('detalhesAparicao');
+            detalhesAparicao.innerHTML = `
+          <h4>${aparicao.Cidade}, ${aparicao.Pais}</h4>
+          <h5>Ano: ${aparicao.Ano}</h5>
+          <p>${aparicao.Descricao}</p>
+          <p><b>Detalhes:</b> ${aparicao.Detalhes}</p>
+        `;
+        }
+    })
+    .catch(error => console.error('Erro ao carregar o JSON:', error));
