5a8f8b559366999ef39c13808d0695574e7aba8e
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-09-08 22:36:33 -0300

    Campo de oração adicionado ao script

--- detalhes.js
+++ detalhes.js
@@ -17,6 +17,8 @@ fetch('aparicoes.json')
           <p>${aparicao.Descricao}</p>
           <p><b>Detalhes:</b> ${aparicao.Detalhes}</p>
           <img src='${aparicao.Imagem_aparicao}' width='300px' height='400px'">
+          <br><br>
+          <p><b>Oração</b><br>${aparicao.oracao}</p>
         `;
         }
     })
