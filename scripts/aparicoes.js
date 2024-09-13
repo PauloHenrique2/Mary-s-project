41f9630a61e614820da1bdacf1518688cc9c5928
DESKTOP-5CO1P0K\Thiago <thiagolutes1@gmail.com>
2024-09-13 18:36:01 -0300

    Arrumando filtro de buscar por nome

--- scripts/aparicoes.js
+++ scripts/aparicoes.js
@@ -60,6 +60,75 @@ document.addEventListener('DOMContentLoaded', function() {
         const digitado = e.target.value;
         clearTimeout(timer);
         timer = setTimeout(() => {
+            filtrarPorNome(digitado)
             console.log(digitado)
         }, 500);
     });
+    
+    
+    
+    
+    
+    function filtrarPorNome(digitado) {
+      fetch('/json/aparicoes.json')
+      .then(response => response.json())
+      .then(data => {
+        const aparicoes = data.Aparicoes;
+
+      digitado = digitado.toLowerCase();
+      const aparicoesFiltradas = aparicoes.filter(aparição => 
+          aparição.Nome.toLowerCase().includes(digitado)
+      );
+  
+      const listaDiv = document.getElementById('aparicoesLista');
+      listaDiv.innerHTML = '';
+  
+      const paisesFiltrados = {};
+  
+      aparicoesFiltradas.forEach(aparição => {
+          if (!paisesFiltrados[aparição.Pais]) {
+              paisesFiltrados[aparição.Pais] = [];
+          }
+          paisesFiltrados[aparição.Pais].push(aparição);
+      });
+  
+      for (const pais in paisesFiltrados) {
+          const section = document.createElement('div');
+          section.classList.add('country-section');
+          section.id = pais.toLowerCase().replace(/ /g, '-');
+          const title = document.createElement('p');
+          title.textContent = pais;
+          title.className = 'country-section-title';
+          section.appendChild(title);
+  
+          paisesFiltrados[pais].forEach(aparição => {
+              const card = document.createElement('a');
+              card.href = `detalhes.html?id=${aparição.id}`;
+              card.classList.add('card');
+              card.innerHTML = `
+                <div class="card-body">
+                  <div class="card-body-left">
+                    <div>
+                      <p class="nomeAparicao">${aparição.Nome}</p>
+                      <img src='${aparição.Imagem_aparicao}' class="imgPrincipalCards">
+                    </div>
+                  </div>
+                  <div class="card-body-right">
+                    <div class="paiTitle">
+                      <div class="center-center" style="flex-direction: column!important; gap: 1.4vh">
+                          <h5 class="card-title">${aparição.Cidade}</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">${aparição.Ano}</h6>
+                      </div>
+                    </div>
+                    <p class="card-text">${aparição.Descricao}</p>
+                  </div>
+                </div>
+              `;
+              section.appendChild(card);
+          });
+  
+          listaDiv.appendChild(section);
+      } 
+    })
+  }
+  
